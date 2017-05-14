a70fbde7e811e94ddc5c789ac6cb46bae23c9fae
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-05-14 12:12:06 +0200

    Added DoctrineFixturesBundle

---
+++ doctrine/doctrine-fixtures-bundle/2.3/src/DataFixtures/ORM/Fixtures.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class Fixtures implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+    }
+}
