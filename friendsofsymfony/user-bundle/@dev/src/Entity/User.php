db02850a9d007afe3778b8370dfc6cc498de6b51
tattali <theoattali@gmail.com>
2018-02-01 08:41:14 +0100

    Add recipe for FOSUserBundle

---
+++ friendsofsymfony/user-bundle/@dev/src/Entity/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
