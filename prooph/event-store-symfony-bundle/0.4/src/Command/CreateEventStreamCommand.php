47394ca8b1cb59620b6fd24d4b46e810beca6c73
Gildas Quéméner <gildas.quemener@gmail.com>
2018-06-19 20:05:11 +0100

    Removed comments from config files

--- prooph/event-store-symfony-bundle/0.4/src/Command/CreateEventStreamCommand.php
+++ prooph/event-store-symfony-bundle/0.4/src/Command/CreateEventStreamCommand.php
@@ -4,14 +4,22 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use Prooph\EventStore\EventStore;
 use Prooph\EventStore\Stream;
 use Prooph\EventStore\StreamName;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class CreateEventStreamCommand extends ContainerAwareCommand
+final class CreateEventStreamCommand extends ContainerAwareCommand
 {
+    private $eventStore;
+
+    public function __construct(EventStore $eventStore)
+    {
+        $this->eventStore = $eventStore;
+    }
+
     protected function configure()
     {
         $this->setName('event-store:event-stream:create')
@@ -21,9 +29,8 @@ class CreateEventStreamCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $eventStore = $this->getContainer()->get('prooph_event_store.app');
+        $this->eventStore->create(new Stream(new StreamName('event_stream'), new \ArrayIterator([])));
 
-        $eventStore->create(new Stream(new StreamName('event_stream'), new \ArrayIterator([])));
         $output->writeln('<info>Event stream was created successfully.</info>');
     }
 }
