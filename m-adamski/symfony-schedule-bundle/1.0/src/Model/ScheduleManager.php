da0b6d7d5ede327f925da64e67c3ad0e2dc1ed2d
Maciej Adamski <m.adamski@ideacode.pl>
2018-10-26 08:46:17 +0200

    Added recipe for Schedule Bundle

---
+++ m-adamski/symfony-schedule-bundle/1.0/src/Model/ScheduleManager.php
@@ -0,0 +1,11 @@
+namespace App\Model;
+
+use Adamski\Symfony\ScheduleBundle\Model\ManagerInterface;
+use Adamski\Symfony\ScheduleBundle\Model\Schedule;
+
+class ScheduleManager implements ManagerInterface {
+
+    public function schedule(Schedule $schedule) {
+        // TODO: ..
+    }
+}
