3d610f70f753cee65e002f3bbfe4acc3be9221f0
Pascal Wacker <pascal.wacker@gmx.ch>
2018-10-27 17:36:10 +0200

    updating msgphp eav receipt for msgphp pull 214

--- msgphp/eav-bundle/0.2/src/Entity/Eav/AttributeValue.php
+++ msgphp/eav-bundle/0.2/src/Entity/Eav/AttributeValue.php
@@ -11,7 +11,7 @@ use MsgPhp\Eav\Entity\AttributeValue as BaseAttributeValue;
  */
 class AttributeValue extends BaseAttributeValue
 {
-    /** @ORM\Id() @ORM\GeneratedValue() @ORM\Column(type="msgphp_attribute_value_id") */
+    /** @ORM\Id() @ORM\GeneratedValue() @ORM\Column(type="msgphp_attribute_value_id", length=191) */
     private $id;
 
     public function __construct(AttributeValueIdInterface $id, Attribute $attribute, $value)
