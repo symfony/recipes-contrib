ddac8ed79bd0b0cca51d25621082cac264e0920a
Yorkie Chadwick <ychadwick@gmail.com>
2019-11-06 10:06:23 +0100

    remove old package comments

--- networking/init-cms-bundle/4.0/src/Entity/Page.php
+++ networking/init-cms-bundle/4.0/src/Entity/Page.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * This file is part of the init_cms_sandbox package.
- *
- * (c) net working AG <info@networking.ch>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
- 
-
 namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
