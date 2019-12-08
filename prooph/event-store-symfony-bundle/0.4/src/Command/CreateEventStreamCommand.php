d2b9a510696a7b2bb7a5d9a8673a99cb825b5bd9
Robin Chalas <robin.chalas@gmail.com>
2019-12-08 22:54:16 +0100

    [prooph/event-store] Fix console command

--- prooph/event-store-symfony-bundle/0.4/src/Command/CreateEventStreamCommand.php
+++ prooph/event-store-symfony-bundle/0.4/src/Command/CreateEventStreamCommand.php
@@ -7,22 +7,26 @@ namespace App\Command;
 use Prooph\EventStore\EventStore;
 use Prooph\EventStore\Stream;
 use Prooph\EventStore\StreamName;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-final class CreateEventStreamCommand extends ContainerAwareCommand
+final class CreateEventStreamCommand extends Command
 {
+    protected static $defaultName = 'event-store:event-stream:create';
+
     private $eventStore;
 
     public function __construct(EventStore $eventStore)
     {
         $this->eventStore = $eventStore;
+
+        parent::__construct(static::$defaultName);
     }
 
     protected function configure()
     {
-        $this->setName('event-store:event-stream:create')
+        $this
             ->setDescription('Create event_stream.')
             ->setHelp('This command creates the event_stream');
     }
