539d89bdafd2ae46bea094a3cd73446d68cbcfba
Baptiste Leduc <baptiste.leduc@gmail.com>
2020-02-17 11:22:30 +0100

    Introduce OpenAPI recipes for JanePHP

---
+++ jane-php/open-api-3/5.3.1/config/jane/open_api.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'openapi-file' => __DIR__ . '/open-api.yaml',
+    'namespace' => 'MyApp\Library\Generated',
+    'directory' => __DIR__ . '/../../generated',
+    'client' => 'psr18',
+];
