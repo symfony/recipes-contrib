1ccb252c1af82cd0a5e3a3462788a98019f43d91
Daniel West <daniel@silverback.is>
2020-04-09 09:33:14 +0100

    Latest update

--- silverback/api-component-bundle/2.0/src/Entity/User.php
+++ silverback/api-component-bundle/2.0/src/Entity/User.php
@@ -6,22 +6,50 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
-use Silverback\ApiComponentBundle\Entity\User\User as BaseUser;
+use Silverback\ApiComponentBundle\Entity\User\AbstractUser;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
 
 /**
  * @author Daniel West <daniel@silverback.is>
  * @ApiResource(
  *     collectionOperations={
- *         "get"={"security"="is_granted('ROLE_SUPER_ADMIN')"},
- *         "post"={"validation_groups"={"Default", "postValidation"}}
+ *         "get"={"security"="is_granted('ROLE_SUPER_ADMIN')"}
  *     },
  *     itemOperations={
- *         "get"={"security"="is_granted('ROLE_SUPER_ADMIN') or object.owner == user"},
- *         "put"={"security"="is_granted('ROLE_SUPER_ADMIN') or object.owner == user"},
+ *         "get"={"security"="is_granted('ROLE_SUPER_ADMIN') or object == user"},
+ *         "put"={"security"="is_granted('ROLE_SUPER_ADMIN') or object == user"},
  *         "delete"={"security"="is_granted('ROLE_SUPER_ADMIN')"}
  *     }
  * )
  * @ORM\Entity
+ * @ORM\Table(name="`user`")
  */
-class User extends BaseUser
-{}
+class User extends AbstractUser
+{
+    public static function loadValidatorMetadata(ClassMetadata $metadata): void
+    {
+        $metadata->addPropertyConstraint('username', new Assert\Email([
+            'message' => 'Please enter a valid email address.',
+        ]));
+    }
+
+    public function __construct(
+        string $username = '',
+        string $emailAddress = '',
+        bool $emailAddressVerified = false,
+        array $roles = ['ROLE_USER'],
+        string $password = '',
+        bool $enabled = true
+    ) {
+        parent::__construct($username, $emailAddress, $emailAddressVerified, $roles, $password, $enabled);
+        $this->emailAddress = $this->username;
+    }
+
+    public function setUsername(?string $username): self
+    {
+        parent::setUsername($username);
+        $this->setEmailAddress($username);
+        return $this;
+    }
+}
