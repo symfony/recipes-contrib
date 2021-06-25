e7896d94e97ee5d5f623a4c4553361159a4273f3
Baptiste Leduc <baptiste.leduc@gmail.com>
2021-06-25 15:17:33 +0200

    [JanePHP] Introduce JSON Schema & OpenAPI bundles

--- jane-php/json-schema/6.0/config/jane/json_schema.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'json-schema-file' => __DIR__ . '/json-schema.json',
-    'root-class' => 'MyModel',
-    'namespace' => 'MyApp\Library\Generated',
-    'directory' => __DIR__ . '/../../generated',
-];
