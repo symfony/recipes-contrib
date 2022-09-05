55e177767faa0c3fb603620bb811d9c9e916c0fd
Thomas Landauer <thomas@landauer.at>
2022-09-06 00:27:45 +0200

    Adding 5.0

---
+++ codeception/codeception/5.0/tests/Support/Helper/Functional.php
@@ -0,0 +1,10 @@
+<?php
+namespace Tests\Support\Helper;
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+class Functional extends \Codeception\Module
+{
+
+}
