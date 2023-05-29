cb4fbd155b3c67602674a976585515c554b3bd8e
Honore Hounwanou <mercuryseries@gmail.com>
2023-05-29 17:12:23 -0400

    fix indentation issue

--- mercuryseries/inertia-bundle/0.1/webpack.ssr.config.js
+++ mercuryseries/inertia-bundle/0.1/webpack.ssr.config.js
@@ -4,39 +4,39 @@ const nodeExternals = require("webpack-node-externals");
 const path = require("path");
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || "dev");
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || "dev");
 }
 
 Encore.addPlugin(new FosRouting())
-  .setOutputPath("public/build-ssr/")
-  .setPublicPath("/build-ssr")
-  .enableReactPreset()
-  .addAliases({
-    "@": path.resolve("assets/js"),
-    "@img": path.resolve("assets/img"),
-  })
-  .addEntry("ssr", "./assets/js/ssr.js")
-  .disableSingleRuntimeChunk()
-  .cleanupOutputBeforeBuild()
-  .enableSourceMaps(!Encore.isProduction())
-  .configureBabel((config) => {
-    // config.plugins.push("@babel/a-babel-plugin");
+    .setOutputPath("public/build-ssr/")
+    .setPublicPath("/build-ssr")
+    .enableReactPreset()
+    .addAliases({
+        "@": path.resolve("assets/js"),
+        "@img": path.resolve("assets/img"),
+    })
+    .addEntry("ssr", "./assets/js/ssr.js")
+    .disableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    .configureBabel((config) => {
+        // config.plugins.push("@babel/a-babel-plugin");
 
-    if (Encore.isProduction()) {
-      config.plugins.push("transform-react-remove-prop-types");
-    }
-  })
-  .configureBabelPresetEnv((config) => {
-    config.useBuiltIns = "usage";
-    config.corejs = "3.23";
-  });
+        if (Encore.isProduction()) {
+            config.plugins.push("transform-react-remove-prop-types");
+        }
+    })
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = "usage";
+        config.corejs = "3.23";
+    });
 
 const config = Encore.getWebpackConfig();
 config.externalsPresets = { node: true };
 config.externals = [
-  nodeExternals({
-    allowlist: [/^@inertiajs/],
-  }),
+    nodeExternals({
+        allowlist: [/^@inertiajs/],
+    }),
 ];
 config.output.globalObject = "this";
 
