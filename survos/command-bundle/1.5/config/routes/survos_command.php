639d35e6f23c01066d64a3677b39a514a200e8cd
Tac Tacelosky <tacman@gmail.com>
2023-10-16 14:31:26 -0400

    cleanup, per Ryan's suggestions

--- survos/command-bundle/1.5/config/routes/survos_command.php
+++ survos/command-bundle/1.5/config/routes/survos_command.php
@@ -1,12 +1,9 @@
 <?php
-// config/routes.php
-
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routes): void {
     $routes->import('@SurvosCommandBundle/config/routes.php')
-        ->prefix('/admin') // consider adding this route to the access_control key in security
-        ->schemes(['https'])
+        ->prefix('/admin') // consider adding this path to the access_control key in security
     ;
 };
 
