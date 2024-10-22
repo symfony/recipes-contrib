0245cc2763dfa22c5122d27177386f164648ea35
johan Vlaar <johan@adivare.nl>
2024-10-22 09:05:47 +0200

    [vincentlanglet/twig-cs-fixer] add default config and add message

---
+++ vincentlanglet/twig-cs-fixer/3.0/.twig-cs-fixer.dist.php
@@ -0,0 +1,21 @@
+<?php
+
+$ruleset = new TwigCsFixer\Ruleset\Ruleset();
+
+// You can start from a default standard
+$ruleset->addStandard(new TwigCsFixer\Standard\TwigCsFixer());
+
+// And then add/remove/override some rules
+//$ruleset->addRule(new TwigCsFixer\Rules\File\FileExtensionRule());
+//$ruleset->removeRule(TwigCsFixer\Rules\Whitespace\EmptyLinesRule::class);
+//$ruleset->overrideRule(
+//    new TwigCsFixer\Rules\Punctuation\PunctuationSpacingRule(
+//        ['}' => 1],
+//        ['{' => 1],
+//    )
+//);
+
+$config = new TwigCsFixer\Config\Config();
+$config->setRuleset($ruleset);
+
+return $config;
