b2784dd7563ab5d2faf9233acd8671b21ab98532
aden <surya.kejawen@gmail.com>
2025-01-30 21:15:47 +0700

    add ad3n jwt refresh bundle

---
+++ ad3n/jwt-refresh-token-bundle/1.0/src/Entity/RefreshToken.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'refresh_tokens')]
+class RefreshToken extends BaseRefreshToken
+{
+}
